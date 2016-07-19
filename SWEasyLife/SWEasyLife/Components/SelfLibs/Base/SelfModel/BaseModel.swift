7ecf6aeadfedee7b3367acef9c053d3902df2736
occ <1321503867@qq.com>
2016-07-19 21:42:46 +0800

    init version

---
+++ SWEasyLife/SWEasyLife/Components/SelfLibs/Base/SelfModel/BaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseModel.swift
+//  SWEasyLife
+//
+//  Created by occ on 16/7/17.
+//  Copyright © 2016年 ox. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+
+}
---
+++ SWEasyLife/SWEasyLife/MainTabBarCtrl.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarCtrl.swift
+//  SWEasyLife
+//
+//  Created by occ on 16/7/17.
+//  Copyright © 2016年 ox. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarCtrl: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+
+
+}

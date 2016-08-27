cc1270e68fe334e0d815268d897e386c4291b757
occ <1321503867@qq.com>
2016-08-27 19:38:57 +0800

    picture

---
+++ SWEasyLife/SWEasyLife/Components/Main/MainPicture/PictureModel/ELMainPictureListModel.swift
@@ -0,0 +1,13 @@
+//
+//  ELMainPictureListModel.swift
+//  SWEasyLife
+//
+//  Created by occ on 16/8/27.
+//  Copyright © 2016年 ox. All rights reserved.
+//
+
+import UIKit
+
+class ELMainPictureListModel: BaseListModel {
+
+}
---
+++ SWEasyLife/SWEasyLife/Components/SelfLibs/Base/SelfModel/BaseListModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseListModel.swift
+//  SWEasyLife
+//
+//  Created by occ on 16/8/27.
+//  Copyright © 2016年 ox. All rights reserved.
+//
+
+import UIKit
+
+class BaseListModel: BaseModel {
+
+}

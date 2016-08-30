230baa83e098c8e5ceeb4fb9680b91cd6db0c0aa
SmileOcc <1950847804@qq.com>
2016-08-30 10:11:36 +0800

    template8

---
+++ SWEasyLife/SWEasyLife/Components/Main/MainHome/HomeView/Template8.swift
@@ -0,0 +1,21 @@
+//
+//  Template8.swift
+//  SWEasyLife
+//
+//  Created by occ on 16/8/30.
+//  Copyright © 2016年 ox. All rights reserved.
+//
+
+import UIKit
+
+class Template8: TemplateBase {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
